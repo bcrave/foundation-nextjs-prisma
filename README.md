23be5929f079842444e2301650f7f4e4b62d3cdc
Brendon Crave <brendoncrave@devpipeline.com>
2022-05-23 18:56:20 +0400

    updated readme, added links to documentation

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ My team at [Dev Pipeline](https://devpipeline.com/) likes to use what we call a 
 functionality and UI necessary for just about any app we need, including a database, routing, authentication, and error handling. The original is built
 with [React](https://reactjs.org/) on the frontend and Python with [Flask](https://flask.palletsprojects.com/en/2.1.x/) on the backend with a [Postgresql](https://www.postgresql.org/)
 database.
+
+I wanted to build it again from scratch using a different tech stack simply to practice using that stack (Next.js, Prisma, Postgres). I plan to build it
+several different times with different stacks as a way to broaden my toolset and observe the different ways a single problem can be solved.
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,37 @@
 import NextLink from "next/link";
 import { Flex } from "@chakra-ui/layout";
-import { Heading, Link } from "@chakra-ui/react";
+import { Heading, Link, Text } from "@chakra-ui/react";
 
 const Home = () => {
   return (
-    <Flex height="80%" justify="center" align="center">
-      <Heading as="h1" size="3xl">
-        <NextLink href="#" passHref>
-          <Link>Welcome to Foundation</Link>
+    <Flex height="80%" justify="center" align="center" direction="column">
+      <Heading as="h1" size="3xl" marginBottom="30px">
+        Welcome to{" "}
+        <NextLink
+          href="https://github.com/bcrave/foundation-nextjs-prisma/blob/main/README.md"
+          passHref
+        >
+          <Link target="_blank" color="#044B7F">
+            Foundation
+          </Link>
         </NextLink>
+      </Heading>
+      <Heading as="h2" fontSize="xl">
+        Dev Pipeline&apos;s Foundation with{" "}
+        <NextLink href="https://nextjs.org/" passHref>
+          <Link color="#044B7F">Next.js</Link>
+        </NextLink>{" "}
+        bootstrapped with{" "}
+        <code
+          style={{
+            backgroundColor: "lightgray",
+            padding: "3px 5px",
+            borderRadius: "10px",
+          }}
+        >
+          create-next-app
+        </code>
+        .
       </Heading>
     </Flex>
   );
