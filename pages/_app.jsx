df1cf2ccc97bcd8bf483f80366dcbc0e3d47b2d5
Brendon Crave <brendoncrave@devpipeline.com>
2022-06-21 21:14:29 +0400

    next-auth installed and provider is working

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,19 +1,22 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import { SessionProvider } from "next-auth/react";
 import "../styles/globals.css";
 
 import Layout from "../components/layout";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
-      {Component.authPage ? (
-        <Component {...pageProps} />
-      ) : (
-        <Layout>
+    <SessionProvider>
+      <ChakraProvider>
+        {Component.authPage ? (
           <Component {...pageProps} />
-        </Layout>
-      )}
-    </ChakraProvider>
+        ) : (
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        )}
+      </ChakraProvider>
+    </SessionProvider>
   );
 }
 
